f5bc35ec5eec7ebccfb4df0e3cbe2e8e11294a53
Jacek Tomaszewski <jacek@jtom.me>
2020-09-22 13:46:43 +0200

    feat: Add `CreateAilornColumnTypeMigration`

---
+++ src/ailorn-utils/CreateAilornColumnTypeMigration.ts
@@ -0,0 +1,13 @@
+import Knex from "knex";
+
+export const CreateAilornColumnTypeMigration: Knex.Migration = {
+  up(knex: Knex): Promise<void> {
+    return knex.raw(
+      "CREATE DOMAIN ailorn AS TEXT CHECK (VALUE ~ '^ailo:w+:w+:w+');"
+    );
+  },
+
+  down(knex: Knex): Promise<void> {
+    return knex.raw("DROP DOMAIN IF EXISTS ailorn;");
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "./ailorn-utils";
 export * from "./history-utils";
 export * from "./KnexRef";
 export * from "./test-utils";
