d18c22f17759142b1f469a073e9985e16d458a3f
Erica Rosalina <erica@ailo.io>
2021-02-22 15:19:02 +1100

    Upgrade to PG 12
    - Resolves time out while waiting for db to be up
    - Resolves `WARNING: The requested image's platform (linux/arm64) does not match the detected host platform (linux/amd64) and no specific platform was requested` in AMD based system

--- bin/local-db.sh
+++ bin/local-db.sh
@@ -69,7 +69,7 @@ function launch_container() {
                     -e POSTGRES_USER="${DB_USER}"           \
                     -e POSTGRES_PASSWORD="${DB_PASSWORD}"   \
                     -p 5432                                 \
-                    -d arm64v8/postgres:11
+                    -d postgres:12
     DB_PORT=$(docker port "${DB_NAME}${DOCKER_SUFFIX}" 5432 | cut -d':' -f2)
 
     echo -e "\nTo run node app/tests with local db, use following env vars:"
